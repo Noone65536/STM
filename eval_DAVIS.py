2f4576c28905e98614cba88a6ce9bc386d79596e
Noone65536 <37800176+Noone65536@users.noreply.github.com>
2021-05-20 11:24:44 +0800

    add comments

--- eval_DAVIS.py
+++ eval_DAVIS.py
@@ -99,7 +99,7 @@ def Run_video(Fs, Ms, num_frames, num_objects, Mem_every=None, Mem_number=None):
 
 
 Testset = DAVIS_MO_Test(DATA_ROOT, resolution='480p', imset='20{}/{}.txt'.format(YEAR,SET), single_object=(YEAR==16))
-Testloader = data.DataLoader(Testset, batch_size=1, shuffle=False, num_workers=2, pin_memory=True)
+Testloader = data.DataLoader(Testset, batch_size=1, shuffle=False, num_workers=2, pin_memory=True) # 一般重写dataset就行了，不用重写dataloader
 
 model = nn.DataParallel(STM())
 if torch.cuda.is_available():
